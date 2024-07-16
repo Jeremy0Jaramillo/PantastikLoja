eb98ed7ee4a98eb63211a447edf98d96372ab342
Jeremy0Jaramillo <jfjaramillo19@utpl.edu.ec>
2024-07-16 10:59:38 -0500

    un cafe

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDz5TIXMvVfKQWWM11VR3L57j-InlXJGz4",
+  authDomain: "pantastikloja.firebaseapp.com",
+  projectId: "pantastikloja",
+  storageBucket: "pantastikloja.appspot.com",
+  messagingSenderId: "189597814690",
+  appId: "1:189597814690:web:a964a8e700960a2122acfb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDz5TIXMvVfKQWWM11VR3L57j-InlXJGz4",
+    authDomain: "pantastikloja.firebaseapp.com",
+    projectId: "pantastikloja",
+    storageBucket: "pantastikloja.appspot.com",
+    messagingSenderId: "189597814690",
+    appId: "1:189597814690:web:a964a8e700960a2122acfb"
+  }
+};
